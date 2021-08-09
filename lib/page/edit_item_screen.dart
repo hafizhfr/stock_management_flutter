508349072833a16fff6e4e7053431db6c89400b3
hafizhfr <hafizhfr33@gmail.com>
2021-08-09 20:43:37 +0700

    edit item fix

--- lib/page/edit_item_screen.dart
+++ lib/page/edit_item_screen.dart
@@ -28,18 +28,24 @@ class _EditItemScreen extends State<EditItemScreen> {
   final itemNameController = TextEditingController();
   final itemCountController = TextEditingController();
   final itemPriceController = TextEditingController();
+  var itemCategoryController = ItemCategoryController();
   final User user = AuthServices.getUser();
 
   bool _validate = false;
 
   @override
-  Widget build(BuildContext context) {
-    //set initial value
+  void initState() {
+    super.initState();
     itemNameController.text = widget.productName;
     itemPriceController.text = widget.productPrice.toString();
     itemCountController.text = widget.productStock.toString();
-    ItemCategoryController itemCategoryController =
+    itemCategoryController =
         ItemCategoryController(itemCategory: widget.productCategory);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //set initial value
 
     var size = MediaQuery.of(context).size;
     final double itemWidth = size.width;
@@ -158,26 +164,28 @@ class _EditItemScreen extends State<EditItemScreen> {
                             gravity: ToastGravity.BOTTOM,
                             fontSize: 16.0,
                           );
-                          // setState(() {
-                          //   _validate = true;
-                          // });
+                          setState(() {
+                            _validate = true;
+                          });
                         } else {
-                          _updateItem(
-                              widget.productName,
-                              itemNameController.text,
-                              int.tryParse(itemCountController.text),
-                              int.tryParse(itemPriceController.text),
-                              itemCategoryController.itemCategory);
+                          setState(() {
+                            _updateItem(
+                                widget.productName,
+                                itemNameController.text,
+                                int.tryParse(itemCountController.text),
+                                int.tryParse(itemPriceController.text),
+                                itemCategoryController.itemCategory);
 
-                          HistoryCollection.addToDB(
-                              itemNameController.text, user.displayName, 2);
+                            HistoryCollection.addToDB(
+                                itemNameController.text, user.displayName, 2);
 
-                          Fluttertoast.showToast(
-                            msg: 'Berhasil mengupdate barang',
-                            toastLength: Toast.LENGTH_LONG,
-                            gravity: ToastGravity.BOTTOM,
-                            fontSize: 16.0,
-                          );
+                            Fluttertoast.showToast(
+                              msg: 'Berhasil mengupdate barang',
+                              toastLength: Toast.LENGTH_LONG,
+                              gravity: ToastGravity.BOTTOM,
+                              fontSize: 16.0,
+                            );
+                          });
                         }
                       },
                     ),
